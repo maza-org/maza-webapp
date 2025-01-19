0e2fd1316f16131d384d6c964c49a61180ecd999
Ivan Bila <ivanbilag@gmail.com>
2025-01-19 12:44:03 +0200

    Add prettier support

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    useTabs: false,
+    tabWidth: 2,
+    jsxSingleQuote: false,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'always',
+    printWidth: 120,
+  };
