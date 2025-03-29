4a0eede12c55819c3e9416926a980e9f9d4377e0
Ivan Bila <ivanbilag@gmail.com>
2025-03-29 23:07:15 +0200

    Change BASE URL

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://maza-strapi-backend.onrender.com/api',
+  baseURL: 'https://api.mazas.org/api',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
