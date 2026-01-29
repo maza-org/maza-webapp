a28743eceed1e8424b4e0c36e0dd8fea8adb60d8
Elio Tovele <eliotoni@outlook.com>
2026-01-29 19:49:02 +0200

    feat: enable auto-capture of app lifecycle events in analytics configuration

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -5,6 +5,7 @@ export const posthogClient = new PostHog(process.env.EXPO_PUBLIC_POSTHOG_API_KEY
   host: 'https://us.i.posthog.com',
   enableSessionReplay: false,
   disabled: __DEV__,
+  captureAppLifecycleEvents: true, // Auto-capture app open/background events
 });
 
 export const identifyAnalyticsUser = (user: User) => {
